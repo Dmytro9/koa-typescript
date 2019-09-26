30eb5ca72057980e3b5834d7392b09939bc1d03b
Dmytro9 <dpoliuliuk@gmail.com>
2019-09-26 15:49:40 +0300

    finish

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,25 +1,60 @@
+import { InMemoryTaskRepository } from './../repositories/inmemorytaskrepository';
+import { TaskService } from './../services/taskservice';
+import { Task } from '../models/task';
 import * as Router from 'koa-router';
 
+
 const router = new Router();
+const service = new TaskService(new InMemoryTaskRepository());
 
 router.get('/tasks', async ctx => {
-    ctx.body = 'Return all tasks';
+    const task = await service.getAllTasks();
+
+    ctx.status = 200;
+    ctx.body = task;
 });
 
 router.get('/tasks/:id', async ctx => {
-    ctx.body = `Return tasks with id ${ctx.params.id}`;
+    const task = await service.getTask(Number(ctx.params.id));
+
+    if ( task ) {
+        ctx.status = 200;
+        ctx.body = task;
+    } else {
+        ctx.throw(404);
+    }
 });
 
 router.post('/tasks', async ctx => {
-    ctx.body = 'Create a new task';
+    const task = <Task>ctx.request.body;
+    const newTask = await service.createTask(task);
+
+    ctx.status = 201;
+    ctx.body = newTask;
+    ctx.set('location', `http://localhost:3005/tasks/${newTask.id}`);
 });
 
 router.put('/tasks/:id', async ctx => {
-    ctx.body = `Update task with id ${ctx.params.id}`;
+    const task = <Task>ctx.request.body;
+    const updatedTask = await service.updateTask(Number(ctx.params.id), task);
+
+    if ( updatedTask ) {
+        ctx.status = 200;
+        ctx.body = updatedTask;
+    } else {
+        ctx.throw(404);
+    }
 });
 
 router.delete('/tasks/:id', async ctx => {
-    ctx.body = `Delete task with id ${ctx.params.id}`;
+    const deletedTask = await service.deleteTask(Number(ctx.params.id));
+
+    if ( deletedTask ) {
+        ctx.status = 200;
+        ctx.body = deletedTask;
+    } else {
+        ctx.throw(404);
+    }
 });
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import * as Koa from 'koa';
 import * as logger from 'koa-logger';
 import * as Router from 'koa-router';
 import taskRouter from './routes/tasks';
-
+import bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new Router();
+const PORT = process.env.PORT || 3000;
 
 app.use(logger());
+app.use(bodyParser());
 
 
 router.get('/', async ctx => {
@@ -19,6 +21,6 @@ app.use(router.routes());
 app.use(taskRouter.routes());
 
 
-app.listen(3001);
+app.listen(PORT);
 
-console.log('My Koa server is app and listening on port 3001');
+console.log(`My Koa server is app and listening on port ${PORT}`);
